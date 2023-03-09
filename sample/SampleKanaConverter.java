71937269368037c77c788e9dd880f044045019cf
AlfaizMomin <92293677+AlfaizMomin@users.noreply.github.com>
2023-03-09 22:20:59 +0530

    Update SampleKanaConverter.java

--- sample/SampleKanaConverter.java
+++ sample/SampleKanaConverter.java
@@ -6,7 +6,6 @@ class SampleKanaConverter
     {
         // Check for arg input
         if(args.length < 1) {
-            System.out.println("Must specify a term to convert");
             System.out.println("EXAMPLE:");
             System.out.println("  java SampleKanaConverter ï¾ƒï½½ï¾„");
             System.out.println("  java SampleKanaConverter ﾃｽﾄ");
