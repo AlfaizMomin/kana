5721f5affe669d0570b305e5364c657df192f266
AlfaizMomin <92293677+AlfaizMomin@users.noreply.github.com>
2023-03-09 22:46:29 +0530

    Update SampleKanaConverter.java

--- sample/SampleKanaConverter.java
+++ sample/SampleKanaConverter.java
@@ -21,7 +21,7 @@ class SampleKanaConverter
         sample_conversion_ops |= KanaConverter.OP_HAN_KATA_TO_ZEN_KATA;
         sample_conversion_ops |= KanaConverter.OP_ZEN_HIRA_TO_ZEN_KATA;
         sample_conversion_ops |= KanaConverter.OP_ZEN_ASCII_TO_HAN_ASCII;
-
+ System.out.println("EXAMPLE:kkk");
         // Convert and print
         String output_string = KanaConverter.convertKana(input_string, sample_conversion_ops);
         System.out.println(output_string);
