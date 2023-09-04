33bba6343dd34474d8aa135788640e51f4980820
AlfaizMomin <alfaiz.momin1998@gmail.com>
2023-09-04 12:35:42 +0530

    Update KanaAppraiserTester.java

--- src/test/java/com/mariten/kanatools/KanaAppraiserTester.java
+++ src/test/java/com/mariten/kanatools/KanaAppraiserTester.java
@@ -5,6 +5,7 @@ public abstract class KanaAppraiserTester
 {
     public KanaAppraiserTester()
     {
+        
         // Test instantiation
         KanaAppraiser kana_evaluator_object = new KanaAppraiser();
     }
