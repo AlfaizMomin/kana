dc03ecadc73219d18535ddcc43dc26263cb674ec
AlfaizMomin <alfaiz.momin1998@gmail.com>
2023-09-15 18:17:27 +0530

    Update KanaConverter.java

--- src/main/java/com/mariten/kanatools/KanaConverter.java
+++ src/main/java/com/mariten/kanatools/KanaConverter.java
@@ -11,6 +11,7 @@ import java.util.Map;
   * http://www.php.net/manual/en/function.mb-convert-kana.php
   */
 public class KanaConverter
+ 
 {
     // Conversion Operations Types
     //// Matched numeric values to originals in PHP's source code
